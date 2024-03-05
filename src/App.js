d7cfc9612f8fac6e91c5337795e811dd039b6521
SoniaCarrilloM <154975385+SoniaCarrilloM@users.noreply.github.com>
2024-03-05 21:13:48 +0100

    added function

--- src/App.js
+++ src/App.js
@@ -1,51 +1,31 @@
-import logo from "./logo.svg";
 import React from "react";
+import Weather from "./Weather";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Weather App This project was coded by
+      <div className="container">
+        <Weather defaultCity="New York" />
+        <footer>
+          This project is created by{" "}
           <a
-            className="linkGit"
             href="https://github.com/SoniaCarrilloM"
             target="_blank"
             rel="noreferrer"
           >
-            Sonia Carrillo
+            Sonia Carrillo{" "}
           </a>
-          , open-sourced on
+          and is{" "}
           <a
-            className="linkRep"
             href="https://github.com/SoniaCarrilloM/weather-app-two-week-4"
             target="_blank"
             rel="noreferrer"
           >
-            GitHub
+            open sourced on GitHub
           </a>
-          and website hosted
-          <a
-            className="linkNetlify"
-            href="https:https://jocular-kringle-228367.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            on Netlify
-          </a>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
--- src/Weather.js
+++ src/Weather.js
@@ -1,16 +1,20 @@
 import Axios from "axios";
 import React from "react";
 
-export default function Weather(props) {
-  function handleResponse(response) {
-    alert(
-      `The weather in ${response.data.main} is ${response.data.main.temp} C`
-    );
-  }
-
-  let apiKey = "3a94f3778290bfeee61278505dbbe51d";
-  let apiUrl = `https://api.openwathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-
-  Axios.get(apiUrl).then(handleResponse);
-  return <h2> Hello from Weather</h2>;
-}
+<div className="Weather">
+  <form>
+    <div className="row">
+      <div className="col-9">
+        <input
+          type="search"
+          placeholder="Enter a city.."
+          className="form-control"
+          autoFocus="on"
+        />
+      </div>
+      <div className="col-3">
+        <input type="submit" value="Search" className="btn btn-primary w-100" />
+      </div>
+    </div>
+  </form>
+</div>;
