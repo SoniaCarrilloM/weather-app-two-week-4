d0641c968dc0224b547bdedf534814f9744e0ae5
SoniaCarrilloM <154975385+SoniaCarrilloM@users.noreply.github.com>
2024-03-05 11:52:43 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,18 +24,20 @@ function App() {
 }
 <footer>
   This project was coded by
-        <a href="https://github.com/SoniaCarrilloM" target="_blank"
-          >Sonia Carrillo</a
-        >, open-sourced on
-        <a
-          href="https://github.com/SoniaCarrilloM/weather-app-two-week-4"
-          target="_blank"
-          >GitHub</a
-        >
-        and website hosted
-        <a href="https:https://jocular-kringle-228367.netlify.app/" target="_blank">
-          on Netlify</a>
-</footer>
-="></a>
+  <a href="https://github.com/SoniaCarrilloM" target="_blank">
+    Sonia Carrillo
+  </a>
+  , open-sourced on
+  <a
+    href="https://github.com/SoniaCarrilloM/weather-app-two-week-4"
+    target="_blank"
+  >
+    GitHub
+  </a>
+  and website hosted
+  <a href="https:https://jocular-kringle-228367.netlify.app/" target="_blank">
+    on Netlify
+  </a>
+</footer>;
 
 export default App;
