d114567df8f831a4144dd3ab6bf8fa1b9e9e6ae5
SoniaCarrilloM <154975385+SoniaCarrilloM@users.noreply.github.com>
2024-03-05 12:06:22 +0100

    added links to paragraph

--- src/App.js
+++ src/App.js
@@ -6,7 +6,35 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Weather App</p>
+        <p>
+          Weather App This project was coded by
+          <a
+            className="linkGit"
+            href="https://github.com/SoniaCarrilloM"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Sonia Carrillo
+          </a>
+          , open-sourced on
+          <a
+            className="linkRep"
+            href="https://github.com/SoniaCarrilloM/weather-app-two-week-4"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+          and website hosted
+          <a
+            className="linkNetlify"
+            href="https:https://jocular-kringle-228367.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            on Netlify
+          </a>
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
