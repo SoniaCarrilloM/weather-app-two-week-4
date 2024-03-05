d71d19404c311d6fd8a84c48d8e318094c46f494
SoniaCarrilloM <154975385+SoniaCarrilloM@users.noreply.github.com>
2024-03-05 10:45:06 +0100

    added links to GitHub and Netlify

--- src/App.js
+++ src/App.js
@@ -22,5 +22,20 @@ function App() {
     </div>
   );
 }
+<footer>
+  This project was coded by
+        <a href="https://github.com/SoniaCarrilloM" target="_blank"
+          >Sonia Carrillo</a
+        >, open-sourced on
+        <a
+          href="https://github.com/SoniaCarrilloM/weather-app-two-week-4"
+          target="_blank"
+          >GitHub</a
+        >
+        and website hosted
+        <a href="https:https://jocular-kringle-228367.netlify.app/" target="_blank">
+          on Netlify</a>
+</footer>
+="></a>
 
 export default App;
