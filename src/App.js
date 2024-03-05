9791b7299fc68d7f6904bd02badb0032b56cfca6
SoniaCarrilloM <154975385+SoniaCarrilloM@users.noreply.github.com>
2024-03-05 12:08:00 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This project was coded by
+          Weather App This project was coded by
           <a
             className="linkGit"
             href="https://github.com/SoniaCarrilloM"
